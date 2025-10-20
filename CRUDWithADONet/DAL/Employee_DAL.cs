186978c7e554ecb0e179db8f8587b713791f6b3b
lab_services_student <lab_services_student@lab7L95SR>
2025-10-20 09:32:04 +0200

    Add project files.

---
+++ CRUDWithADONet/DAL/Employee_DAL.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Windows.Input;
+using CRUDWithADONet.Models;
+using Microsoft.Data.SqlClient;
+namespace CRUDWithADONet.DAL
+{
+    public class Employee_DAL
+    {
+
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        public static IConfiguration Configuration { get; set; }
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            return Configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        public List<Employee> GetAll() { 
+        List<Employee> employeeList = new List<Employee>();
+        using (_connection = new SqlConnection(GetConnectionString())){
+        _command = _connection.CreateCommand();
+        _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[DBO].[usp_Get_Employees]";
+        
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+        while (dr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.ID = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]);
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["Salary"]);
+                    employeeList.Add(employee);
+                }
+        _connection.Close();
+        }
+        return employeeList;
+    }
+}}
---
+++ CRUDWithADONet/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDWithADONet.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        public int ID {  get; set; }
+        [DisplayName("FirstName Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [DisplayName("Date Of Birth")]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [DisplayName("Email")]
+        [Required]
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        public string FullName {  
+        get { return FirstName + " " + LastName; }
+        
+        }
+
+
+
+
+    }
+}
